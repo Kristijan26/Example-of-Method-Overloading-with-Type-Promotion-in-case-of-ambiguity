1ae96a7fb8552f85d488dac7f9175342c521fbc0
Kritijan <kbojlevski41@gmail.com>
2024-04-22 17:40:22 +0200

    Case of ambiguil

---
+++ src/main/java/Caseofambiguity.java
@@ -0,0 +1,9 @@
+class Caseofambiguity{  
+  void sum(long a,long b){System.out.println("a method invoked");}  
+  void sum(long a,int b){System.out.println("b method invoked");}  
+  
+  public static void main(String args[]){  
+	  Caseofambiguity obj=new Caseofambiguity();  
+  obj.sum(20,20);//now ambiguity  
+  }  
+} 
